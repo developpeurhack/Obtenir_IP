920aba5fae9aaa283da391058692a767c0f5315b
moh <msn.moise@hotmail.fr>
2021-05-04 09:58:15 +0200

    fichier php

---
+++ index1.php
@@ -0,0 +1,18 @@
+
+<?php
+
+
+// Comment recuperer IP d'un Client on evitant les Proxy
+
+
+  function getIp(){
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+      $ip = $_SERVER['HTTP_CLIENT_IP'];
+    }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+      $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }else{
+      $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+  }
+  echo 'L adresse IP de l utilisateur est : '.getIp();
